3c1f582087567ce60d7a01fb9fa7185775297e01
Brady <vanderbannond@gmail.com>
2018-09-10 00:34:08 -0700

    seeds working

---
+++ db/seeds/01_users.js
@@ -0,0 +1,25 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { id: 1,
+          email: 'alborg.b@gmail.com',
+          name: 'Brady',
+          password: 'pass',
+          pub_key: '0x23571754544Cc1a95d269B8D75bAFf4C17A8B223'},
+        { id: 2,
+          email: 'test@gmail.com',
+          name: 'Greg',
+          password: 'pass',
+          pub_key: '0xC8a013451768a24177926f6E27f938909A2C493E'},
+        { id: 3,
+          email: 'test@test.com',
+          name: 'Nicki',
+          password: 'pass',
+          pub_key: '0x13e8f455058885ac53896cd252880D153993B399'},
+      ]);
+    });
+};
